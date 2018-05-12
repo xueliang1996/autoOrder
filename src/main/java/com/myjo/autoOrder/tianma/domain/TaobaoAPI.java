ec1d92a6e966b22a6b9a40a3ce3c438861bf17ce
xueliang1996 <33866510+xueliang1996@users.noreply.github.com>
2018-05-12 17:32:38 +0800

    Update TaobaoAPI.java

--- src/main/java/com/myjo/autoOrder/tianma/domain/TaobaoAPI.java
+++ src/main/java/com/myjo/autoOrder/tianma/domain/TaobaoAPI.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class TaobaoAPI {
 	private String taobaoUrl = "http://gw.api.taobao.com/router/rest";
-	private String appkey = "23279400";
-	private String secret = "7cb1d50fc70c7548b31d414c2adbae06";
-	private String sessionKey = "610061860172d8cb9f255a51961f5d4885daaff6a5bad72738840638";
+	private String appkey = "";
+	private String secret = "";
+	private String sessionKey = "";
 
 	public String getTaobaoUrl() {
 		return taobaoUrl;
